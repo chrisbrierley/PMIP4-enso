c03fc33c6c08a50d7870ee11417a1db44741ac42
Chris Brierley <c.brierley@ucl.ac.uk>
2019-08-05 00:08:01 +0100

    added some running location flexibility into download data script

--- scripts/download_cvdp_enso_data.sh
+++ scripts/download_cvdp_enso_data.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #This file will download/update the input netcdf files that haave been created from running CVDP on PMIP4 
 
-cd ../data
+#move to data directory
+this_dir=`basename $PWD`
+if [ $this_dir == "scripts" ]
+then 
+  cd ../data
+else
+  cd data
+fi
+
 wget http://www2.geog.ucl.ac.uk/~ucfaccb/PMIPVarData/data/PMIP4_cvdp_enso.tar.gz
 tar -xvf PMIP4_cvdp_enso.tar.gz
